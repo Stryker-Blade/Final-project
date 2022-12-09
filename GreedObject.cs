24019263218a79a5d54447d9235e48dacb4faa72
Stryker-Blade <jensen.d.call@gmail.com>
2022-12-09 12:40:11 -0700

    Added in the code

---
+++ GreedObject.cs
@@ -0,0 +1,147 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Helloworld{
+class Colors{
+
+    protected List<Color> colorNames = new List<Color>();
+
+    public Colors(){
+        colorNames.Add(Color.RED);
+        colorNames.Add(Color.ORANGE);
+        colorNames.Add(Color.YELLOW);
+        colorNames.Add(Color.GREEN);
+        colorNames.Add(Color.BLUE);
+        colorNames.Add(Color.PURPLE);
+        colorNames.Add(Color.WHITE);
+    }
+    public Color randomColor(){
+        Random random = new Random();
+        int index = random.Next(0,6);
+        Color color = colorNames[index];
+        return color;
+
+    }
+}
+class ColoredObject: Colors  {
+    public Color Color { get; set; }
+
+    public ColoredObject (){
+        Color = this.randomColor();
+    }
+
+}
+
+
+
+class FallingObject: ColoredObject{
+
+    public Vector2 Position { get; set; } = new Vector2(0, 0);
+    public Vector2 Velocity { get; set; } = new Vector2(0, 0);
+
+    public int pointvalue {get; set; }= 0;
+
+    public virtual bool CheckCollision(Rectangle rect){
+        return false;
+    }
+
+    virtual public void Draw(){
+
+    }
+    public void Move() {
+        Vector2 NewPosition = this.Position;
+        NewPosition.X += Velocity.X;
+        NewPosition.Y += Velocity.Y;
+        this.Position = NewPosition;
+    }
+    }
+
+class Gem: FallingObject{
+
+    public int Size {get; set;} 
+
+    public override bool CheckCollision(Rectangle rect){
+        return Raylib.CheckCollisionRecs(rect,CollisonGem());
+    }
+    public Gem(int size){
+        Size = size;
+        pointvalue = -1;
+    }
+    public Rectangle CollisonGem(){
+        return new Rectangle((int)Position.X, (int)Position.Y, Size, Size);
+    }
+    
+
+    override public void Draw() {
+        Raylib.DrawRectangleRec(CollisonGem(), Color);
+    }
+}
+
+class Rock: FallingObject{
+    public int Radius { get; set; }
+
+    public Rock( int radius){
+        Radius = radius;
+        pointvalue = 1;
+    }
+    public override bool CheckCollision(Rectangle rect){
+        return Raylib.CheckCollisionCircleRec(Position,Radius,rect);
+    }
+    override public void Draw() {
+        Raylib.DrawCircle((int)Position.X, (int)Position.Y, Radius, Color);
+    }
+
+}
+
+class Player{
+    public Rectangle PlayerPosition {get;set;} = new Rectangle(400, 460,20,20);
+
+        public void movement(){
+            var newplayerposition = PlayerPosition;
+            var PlayerMovementSpeed = 10;
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+                    newplayerposition.x += PlayerMovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+                    newplayerposition.x -= PlayerMovementSpeed;
+                }
+                if (newplayerposition.x < 0){
+                    newplayerposition.x = 0;
+                }
+                if (newplayerposition.x > 780){
+                    newplayerposition.x = 780;
+                }
+                PlayerPosition = newplayerposition;
+                Raylib.DrawRectangleRec(PlayerPosition, Color.MAROON);
+            }   
+        }
+
+class Projectile: FallingObject{
+    public int Size {get;set;}
+
+    public override bool CheckCollision(Rectangle rect){
+        return Raylib.CheckCollisionRecs(rect,CollisonProjectile());
+    }
+    public Projectile(int size){
+        Size = size;
+    }
+    public Rectangle CollisonProjectile(){
+        return new Rectangle((int)Position.X, (int)Position.Y, Size, Size);
+    }
+    override public void Draw() {
+        Raylib.DrawRectangleRec(CollisonProjectile(), Color.GOLD);
+    }
+
+}
+class Score{
+
+    public int pointtotal {get; set;} = 0;
+    public void scorekeeping(){
+    Raylib.DrawText($"{pointtotal}",20,20,20,Color.WHITE);
+    }
+}    
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,140 @@
+using Raylib_cs;
+using System.Numerics;
+
+namespace Helloworld{
+static class Program{
+        public static void Main()
+        {
+
+            var ScreenHeight = 480;
+            var ScreenWidth = 800;
+            var Objects = new List<FallingObject>();
+            var projectilelist = new List<Projectile>();
+            var Random = new Random();
+            var player = new Player();
+            var score = new Score();
+            int lives = 15;
+            var playerPosition = player.PlayerPosition;
+            var projectilecollision = new Projectile(2);
+
+            Raylib.InitWindow(ScreenWidth, ScreenHeight, "Greed");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                // Add a new random object to the screen every iteration of our game loop
+                var whichType = Random.Next(100);
+
+                // Generate a random velocity for this object
+                var randomY = Random.Next(1, 4);
+                var Rocketspeed = Random.Next(3, 6);
+                var randomX = Random.Next(1, 4);
+                
+                //Generating a random x coordinate for each object
+                var xposition = Random.Next(20,780);
+                
+                // I am generating the positions of the falling objects here
+                var position = new Vector2(xposition, 0);
+                var position2 = new Vector2(xposition,480);
+            
+                switch (whichType) {
+                            case 0:
+                            //Tempararoly using squares for gem, need to switch to actual drawing
+                                var gem = new Gem(20);
+                                gem.Position = position2;
+                                gem.Velocity = new Vector2(0, -Rocketspeed);
+                                Objects.Add(gem);
+                                break;
+                        case 1:
+                        //Tempararoly using circles for rock, need to switch to actual drawing
+                            var rock = new Rock(15);
+                            rock.Position = position;
+                            rock.Velocity = new Vector2(0, randomY);
+                            Objects.Add(rock);
+                            break;
+                        case 2:
+                            rock = new Rock(15);
+                            rock.Position = position;
+                            rock.Velocity = new Vector2(0, randomY);
+                            Objects.Add(rock);
+                            break;
+                        case 3:
+                            rock = new Rock(15);
+                            rock.Position = position;
+                            rock.Velocity = new Vector2(0, randomY);
+                            Objects.Add(rock);
+                            break;
+                        case 4:
+                            rock = new Rock(15);
+                            rock.Position = position;
+                            rock.Velocity = new Vector2(0, randomY);
+                            Objects.Add(rock);
+                            break;
+                        default:
+                            break;
+                        
+                        }
+                    Raylib.BeginDrawing();
+                    Raylib.ClearBackground(Color.BLACK);
+                    // This draws all of the objects in their current location, updating every single runthrough of the program
+                   
+                   if(Raylib.IsKeyDown(KeyboardKey.KEY_UP)){
+                        var projectile = new Projectile(5);
+                        projectile.Position = new Vector2(player.PlayerPosition.x+10, player.PlayerPosition.y-30);
+                        projectile.Velocity = new Vector2(0,-15);
+                        projectilelist.Add(projectile);
+                        
+                   }
+
+
+
+                    foreach (var obj in Objects) {
+                        obj.Draw();
+                    }
+                    foreach (var projectile in projectilelist) {
+                        projectile.Draw();
+                        projectile.Move();
+                    }
+
+                    player.movement();
+                
+                    // place to add in the projectile loop
+                    score.scorekeeping();
+                    // Move all of the objects to their next location
+                    Raylib.DrawText($"You have {lives} lives left remaining", 430,20,20,Color.WHITE);
+                    foreach (var obj in Objects.ToList()) {
+                        obj.Move();
+                        foreach(var projectile in projectilelist.ToList()){
+                            if(obj.CheckCollision(projectile.CollisonProjectile())){
+                            var newscore = score.pointtotal +  obj.pointvalue;
+                            score.pointtotal = newscore;
+                            Objects.Remove(obj);
+                            projectilelist.Remove(projectile);
+                            }
+                        }
+                        if (obj.Position.Y > 490){
+                            Objects.Remove(obj);
+                            lives = lives - 1;
+                            if (lives.Equals(0)){  
+                                Raylib.CloseWindow();
+                            }
+                        if(obj.Position.Y < 0){
+                            Objects.Remove(obj);
+                        }
+                        
+                        
+                        }
+                    }
+                Raylib.EndDrawing();
+            }
+        
+            
+
+
+        }
+        
+}
+            
+}
+
+
